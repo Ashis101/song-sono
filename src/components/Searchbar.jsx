9418e89d5de195b0350e93269247045afc6f579c
Ashis Banerjee <58388611+Ashis101@users.noreply.github.com>
2023-06-28 22:20:08 +0530

    search functioality completed

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,12 @@
-const Searchbar = () => (
-  <div>Loader</div>
+import {HiSearch} from "react-icons/hi";
+
+const Searchbar = ({setsearch}) => (
+
+  <div className="w-full relative">
+      <input className="w-full p-6 border-none outline-none bg-black bg-opacity-100 text-white rounded-md text-xl" type="text" placeholder="Search" onInput={(e)=>{setsearch(e.target.value.toLowerCase())}} />
+      <HiSearch className="text-[50px] text-white absolute top-3 right-5 "/>
+  </div>  
+
 );
 
 export default Searchbar;
