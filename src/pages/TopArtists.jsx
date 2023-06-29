3677f0b9c418f47553b9c1d1a798955bb972e285
Ashis Banerjee <58388611+Ashis101@users.noreply.github.com>
2023-06-29 08:50:49 +0530

    add loader and design artist section

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,26 @@
-const TopArtists = () => <div>TopArtists</div>;
+import {useGetTopchartsQuery} from '../redux/services/shazam_core'
+import { Link } from 'react-router-dom'; 
+import { Loader } from '../components';
+const TopArtists = () =>{
+    const {data,isFetching,error}=useGetTopchartsQuery();
+    if(isFetching) return <Loader title="Loading Artists...."/>
+
+    let artists_data=data?.tracks
+    return(
+        <div className='w-full h-full'>
+            <div className='w-full max-h-full grid grid-cols-4 gap-x-8 gap-y-4 place-items-center p-3'>
+            {
+                artists_data?.map((data)=>( 
+                    <Link className="w-24 h-24 md:w-48 md:h-auto   " to={`/artists/${data?.artists[0].adamid}`} >
+                    <img className='className="w-24 h-24 rounded-full' src={data?.images.background} />
+                    </Link> 
+                
+              ))
+            }
+            </div>
+           
+        </div>
+    )
+}
 
 export default TopArtists;
